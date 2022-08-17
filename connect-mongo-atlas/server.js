ae6cd23af0fe04dd91e75b70124a860be27eab58
sanlokaja <sandyloka790@gmail.com>
2022-08-17 23:41:51 +0700

    materials/endpoint-CRUD-system

--- connect-mongo-atlas/server.js
+++ connect-mongo-atlas/server.js
@@ -6,29 +6,109 @@ const app = express();
 app.use(express.json());
 
 
-/* --- Import Model --- */
+/* ------------------ Import Model ------------------ */
 
 const postModel = require('./postModel');
 
-/* --- End Import Model --- */
+/* ------------------ End Import Model ------------------ */
 
 
-/* --- Define API --- */
+/* ------------------ Define API ------------------ */
+
+
+/* --- Create Post --- */
 
 app.post('/v1/create', async(req, res) => {
 
     const {title, content} = req.body;
 
     try{
+
         const newPost = await postModel.create({title, content});
         res.json(newPost);
+
     } catch (err){
         res.status(500).send(err);
     };
     
 });
 
-/* --- End Define API --- */
+/* --- End Create Post --- */
+
+
+/* --- Get Post --- */
+
+app.get("/v1/posts", async(req, res) => {
+    try{
+
+        const getPosts = await postModel.find();
+        res.json(getPosts);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+/* --- End Get Post --- */
+
+/* --- Get Post By Id --- */
+
+app.get("/v1/posts/:id", async(req, res) => {
+    
+    const { id } = req.params;
+    
+    try{
+
+        const getPostById = await postModel.findById(id);
+        res.json(getPostById);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+/* --- End Get Post By Id --- */
+
+
+/* --- Update Post By Id --- */
+
+app.put("/v1/posts/update/:id", async(req, res) =>{
+    
+    const { id } = req.params;
+    const { title, content } = req.body;
+    
+    try{
+
+        const updateById = await postModel.findByIdAndUpdate(id, { title, content });
+        res.json(updateById);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+/* --- End Update Post By Id --- */
+
+
+/* --- Delete Post By Id --- */
+
+app.delete("/v1/posts/delete/:id", async(req, res) =>{
+    
+    const { id } = req.params;
+    
+    try{
+
+        const deleteById = await postModel.findByIdAndDelete(id);
+        res.json('Delete success!');
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+/* --- End Delete Post By Id --- */
+
+/* ------------------ End Define API ------------------ */
 
 app.listen(PORT, () => {
     console.log(`Server berhasil berjalan di port http://localhost:${PORT}`);
