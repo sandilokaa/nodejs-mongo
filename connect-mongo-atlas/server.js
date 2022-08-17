9ed7a204aef8b0f95b0ca8110f60ae0bdaa11a8c
sanlokaja <sandyloka790@gmail.com>
2022-08-17 14:35:36 +0700

    materials/add-post-endpoint

--- connect-mongo-atlas/server.js
+++ connect-mongo-atlas/server.js
@@ -5,6 +5,8 @@ const PORT = 2000;
 const app = express();
 app.use(express.json());
 
+/* --- Connect to Mongo Atlas --- */
+
 mongoose.connect(
     "mongodb://sanlokngab:sanlokngab123@ac-lpthsvt-shard-00-00.8dl27mt.mongodb.net:27017,ac-lpthsvt-shard-00-01.8dl27mt.mongodb.net:27017,ac-lpthsvt-shard-00-02.8dl27mt.mongodb.net:27017/mongoPractice?ssl=true&replicaSet=atlas-e8i0st-shard-0&authSource=admin&retryWrites=true&w=majority",
     {
@@ -12,10 +14,37 @@ mongoose.connect(
         useUnifiedTopology: true
     }
 ).then(() => {
-    console.log("Connected");
+    console.log("Connected to cloud!");
 }).catch((err) => {
     console.log(err);
 });
+
+/* --- End Connect to Mongo Atlas --- */
+
+
+/* --- Import Model --- */
+
+const postModel = require('./postModel');
+
+/* --- End Import Model --- */
+
+
+/* --- Define API --- */
+
+app.post('/v1/create', async(req, res) => {
+
+    const {title, content} = req.body;
+
+    try{
+        const newPost = await postModel.create({title, content});
+        res.json(newPost);
+    } catch (err){
+        res.status(500).send(err);
+    };
+    
+});
+
+/* --- End Define API --- */
 
 app.listen(PORT, () => {
     console.log(`Server berhasil berjalan di port http://localhost:${PORT}`);
