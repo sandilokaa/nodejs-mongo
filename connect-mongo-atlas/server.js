51707a0cf6597cfbd028524fd6a5c54fa94b8e8f
sanlokaja <sandyloka790@gmail.com>
2022-08-17 22:31:13 +0700

    materials/import-mongoose-connect

--- connect-mongo-atlas/server.js
+++ connect-mongo-atlas/server.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongooseConnect = require("./connection");
 
 const PORT = 2000;
 const app = express();
 app.use(express.json());
-
-/* --- Connect to Mongo Atlas --- */
-
-mongoose.connect(
-    "mongodb://sanlokngab:sanlokngab123@ac-lpthsvt-shard-00-00.8dl27mt.mongodb.net:27017,ac-lpthsvt-shard-00-01.8dl27mt.mongodb.net:27017,ac-lpthsvt-shard-00-02.8dl27mt.mongodb.net:27017/mongoPractice?ssl=true&replicaSet=atlas-e8i0st-shard-0&authSource=admin&retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(() => {
-    console.log("Connected to cloud!");
-}).catch((err) => {
-    console.log(err);
-});
-
-/* --- End Connect to Mongo Atlas --- */
 
 
 /* --- Import Model --- */
